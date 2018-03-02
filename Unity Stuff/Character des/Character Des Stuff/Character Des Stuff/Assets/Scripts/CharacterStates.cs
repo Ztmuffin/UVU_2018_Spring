0d3821de41ad59aaac057ce437361222bccd8c43
Unknown <addisonbmoyer@gmail.com>
2018-03-01 21:04:51 -0700

    Scriptible objects research Somewhat working
    See all committed files under Character Des stuff

---
+++ Unity Stuff/Character des/Character Des Stuff/Character Des Stuff/Assets/Scripts/CharacterStates.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class CharacterStates : ScriptableObject
+{
+	public new string name;
+	public string description;
+
+	public GameObject Pose;
+	
+
+
+
+}
