223aba22d2c5dbc1b04ee2263d9530e760fe0d68
Unknown <addisonbmoyer@gmail.com>
2018-03-06 10:14:10 -0700

    Scriptable objects working better

--- Unity Stuff/Character des/Character Des Stuff/Character Des Stuff/Assets/OnStateExitB.cs
+++ Unity Stuff/Character des/Character Des Stuff/Character Des Stuff/Assets/OnStateExitB.cs
@@ -5,29 +5,20 @@ using UnityEngine;
 
 public class OnStateExitB : StateMachineBehaviour {
 
-	 // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
-	//override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
-	//
-	//}
-
-	// OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
-	//override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
-	//
-	//}
+	// OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
+	public GameEvent EnterEvent;
+	
+	override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+		EnterEvent.Raise();
+	
+	}
+	
 
 	// OnStateExit is called when a transition ends and the state machine finishes evaluating this state
-	public GameEvent Event;
+	public GameEvent ExitEvent;
 	override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
-		Event.Raise();
+		ExitEvent.Raise();
 	}
 
-	// OnStateMove is called right after Animator.OnAnimatorMove(). Code that processes and affects root motion should be implemented here
-	//override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
-	//
-	//}
 
-	// OnStateIK is called right after Animator.OnAnimatorIK(). Code that sets up animation IK (inverse kinematics) should be implemented here.
-	//override public void OnStateIK(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
-	//
-	//}
 }
